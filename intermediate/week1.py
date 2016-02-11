27cde975e20965ca7cdf4d2d266734b6befc266c
Ilê Caian <caian.gums@grad.ufsc.br>
2016-02-11 12:24:30 -0200

    workin!

--- intermediate/week1.py
+++ intermediate/week1.py
@@ -1,65 +1,110 @@
 # caian 05/01/2016
 
-class Date(object):
+class Hour(object):
     """
-    Simple date object.
+    Simple hour object.
     """
-    def __init__(self, day, month, year):
-        self.day = day
-        self.month = month
-        self.year = year
+    def __init__(self, hour, minutes, seconds):
+        self.hour = hour
+        self.minutes = minutes
+        self.seconds = seconds
 
-    def today(self):
+    def moment(self):
         """
-        Return a string date of today.
+        Return a string of the moment.
         """
-        return str(self.day + "/" + self.month + "/" + self.year)
+        return str(self.hour) + ":" + str(self.minutes) + ":" + str(self.seconds)
 
 class Event():
     """
     Simple event object.
     """
-    def __init__(self, name, date, description=None):
+    def __init__(self, name, hour, description=None):
         self.name = name
-        self.date = date
+        self.hour = hour
         self.description = "No description."
         if description != None:
             self.description = description
 
     @property
     def show(self):
-       return self.date.today() + "   " + self.name + "\n" + self.description + "\n"
+       return self.hour.moment() + "   " + self.name + "\n" + self.description + "\n"
         
-class Calendar(object):
+class Scheduler(object):
     """
-    Simple calendar.
+    Simple scheduler.
     """
-    def __init__(self, date_today):
-        self.date = date_today
+    def __init__(self):
         self.events = {}
         self.count = 0
 
-    def today(self):
-        """
-        Return a string date of today.
-        """
-        return self.date.today
-
-    def schedule(self, name, date, description=None):
+    def schedule(self, name, hour, description=None):
         """
         Schedule some event.
         """
-        event = Event(name, date, description)
+        event = Event(name, hour, description)
         self.count += 1
-        events[self.count] = event
-        print("Event: " + self.count + event.show)
+        self.events[self.count] = event
+        print("Event: " + str(self.count) + "  " + str(event.show))
         return
 
     def cancel(self, name):
         """
         Cancel a schedule event.
         """
-        pass
+        for each in list(self.events.keys()):
+            if self.events[each].name == name:
+                del self.events[each]
+        return
+
+    def show_events(self):
+        """
+        Show the schedule events
+        """
+        r = "Events:\n"
+        for each in list(self.events.keys()):
+            r += str(each) + "   " + self.events[each].show + "\n"
+        return r
 
 def menu():
-    pass
+    op = -1
+    while(op < 0 or op > 4):
+        print("What do you wanna do?")
+        print("  0 - exit")
+        print("  1 - show scheduled events")
+        print("  2 - create a new event")
+        print("  3 - cancel a event")
+        print("  4 - ...(future features?)")
+        op = int(input("> "))
+    return op
+
+def create_event():
+    n = input("What's the name of event?\n> ")
+    h = input("What's the hour of event?(hour, minute, second)\n> ")
+    m = input("> ")
+    s = input("> ")
+    des = input("What's the description of event?\n> ")
+
+    hr = Hour(h, m, s)
+    sch_ev = Event(n, hr, des)
+    return sch_ev
+
+"""
+Program starts here.
+"""
+print("This is a simple Scheduler - version 0.1")
+option = -1
+sch = Scheduler()
+while(option != 0):
+    option = menu()
+    if option == 1:
+        ev = sch.show_events()
+        print(ev)
+    elif option == 2:
+        ne = create_event()
+        sch.schedule(ne.name, ne.hour, ne.description)
+    elif option == 3:
+        name = input("What's the event name:\n> ")
+        sch.cancel(name)
+        
+print("bye!")
